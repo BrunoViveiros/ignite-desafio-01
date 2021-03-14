91108e3c473fcda3f4c4c3211d03401020af3683
Bruno Viveiros <bruferviveiros@gmail.com>
2021-03-14 19:04:25 -0300

    Conclusao do Desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,37 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle.trim()) {
+      const newTask = {
+        id: Math.floor(Math.random() * 10000),
+        title: newTaskTitle.trim(),
+        isComplete: false
+      }
+
+      setTasks([...tasks, newTask]);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(task => {
+      if(task.id === id) {
+        task.isComplete = !task.isComplete;
+      }
+
+      return task;
+    }));
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTaskList = tasks.filter(task => {
+      if(task.id !== id) {
+        return task;
+      }
+    })
+    
+    setTasks(newTaskList);
   }
 
   return (
